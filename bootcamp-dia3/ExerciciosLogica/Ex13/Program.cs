62b4277c331ba2e5375c122eb9867f1cb9995713
Monique Almirante <monique.kinha@gmail.com>
2026-02-01 11:40:15 -0300

    Atualização Exercícios

---
+++ bootcamp-dia3/ExerciciosLogica/Ex13/Program.cs
@@ -0,0 +1,44 @@
+﻿//entrada de usuário
+
+Console.WriteLine("Digite a sua altura: ");
+
+double altura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o seu sexo (M para masculino e F para feminino)");
+
+string sexo = Console.ReadLine().ToUpper();
+
+Console.WriteLine("Digite seu peso atual: ");
+
+double peso = Convert.ToDouble(Console.ReadLine());
+
+double pesoIdeal = 0;
+
+//calculo peso ideal
+if (sexo == "M")
+{
+    pesoIdeal = (72.7 * altura) - 58;
+
+} else if (sexo == "F")
+{
+    pesoIdeal = (62.1 * altura) - 44.7;
+} else
+{
+    Console.WriteLine("Sexo inválido. Use F ou M");
+    return;
+}
+
+
+//verifica se o peso do usuario ta dentro do peso ideal
+
+if (peso < pesoIdeal)
+{
+    Console.WriteLine("Seu peso está abaixo do peso ideal.");
+
+} else if (peso > pesoIdeal)
+{
+    Console.WriteLine("Seu peso está acima do peso ideal.");
+}else
+{
+    Console.WriteLine("Você está dentro do peso ideal.");
+}
---
+++ bootcamp-dia3/ExerciciosLogica/Ex14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.PortableExecutable;
+
+
+Console.WriteLine("Obtendo peso do peixe...");
+//pede o peso do peixe e pega a variavel
+Console.WriteLine("Digite abaixo o peso do peixe mostrado: ");
+double pesoPeixe = Convert.ToDouble(Console.ReadLine());
+
+//declara as variáveis e da a condição para saber se o peixe está dentro do peso estabelecido
+double excesso = 0;
+double multa = 0;
+
+if (pesoPeixe > 50)
+{
+    excesso = pesoPeixe - 50;
+    multa = excesso * 4;
+    Console.WriteLine($"Excesso em Kg: {excesso}");
+    Console.WriteLine($"O valor da multa é: {multa}");
+} else
+{
+    Console.WriteLine("O peixe não excedeu o peso. O valor da multa é zero.");
+}
+
+
