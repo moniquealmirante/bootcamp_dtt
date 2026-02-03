1fcdb3d1b4ec5ac1a42776a723e9956a39434e4b
aluno <aluno@embarquedigital.portodigital.local>
2026-02-03 17:44:31 -0300

    case3.1

---
+++ bootcamp-dia5/case3.1/Visitante.cs
@@ -0,0 +1,16 @@
+
+
+using System.Security.Cryptography.X509Certificates;
+
+class Visitante
+{
+    public string Nome {get; }
+    public string Documento {get; }
+    public DateTime HorarioChegada {get ;}
+    public bool EhPrimeiraVez {get; }
+    
+}
+
+
+
+
